393708675189d9d4085e5e18c328d07ff1287425
eunbin55 <eunbin620@naver.com>
2025-08-21 18:18:11 +0900

    feat: roadmap schema

---
+++ src/models/Roadmap.ts
@@ -0,0 +1,37 @@
+import { Schema } from "mongoose";
+
+const resourceSchema = new Schema({
+  resourceType: { type: String, enum: ["course", "study"], default: "course" },
+  name: { type: String },
+  location: { type: String },
+  price: { type: String },
+  rating: { type: Number },
+  provider: { type: String },
+});
+
+const pathSchema = new Schema(
+  {
+    title: { type: String },
+    description: { type: String },
+    skills: { type: [String] },
+    duration: { type: String },
+    resources: [resourceSchema],
+  },
+  { _id: true }
+);
+
+const roadmapSchema = new Schema(
+  {
+    userId: { type: String, required: true },
+    plan: {
+      type: String,
+      enum: ["3months", "6months", "1year"],
+      default: "3months",
+    },
+    paths: [pathSchema],
+  },
+  { timestamps: true }
+);
+
+const Roadmap = mongoose.model("Roadmap", roadmapSchema);
+export { Roadmap };
