39783d9c6019690fb487d1537c7526fd50bf6dc3
eunbin55 <eunbin620@naver.com>
2025-08-22 19:23:58 +0900

    feat: createRoadmap

---
+++ src/controllers/roadmap.controller.ts
@@ -0,0 +1,44 @@
+import { Resume } from "@models/Resume";
+import { Roadmap } from "@models/Roadmap";
+import { Request, Response } from "express";
+import geminiController from "./gemini.controller";
+import { User } from "@models/User";
+
+const roadmapController = {
+  createRoadmap: async (req: Request, res: Response) => {
+    try {
+      const resumeId = req.params.id;
+      const resume = await Resume.findById(resumeId).populate<{ userId: User }>(
+        {
+          path: "userId",
+          model: "User",
+          select: "major career skill region interestJob level",
+        }
+      );
+      // 1. resume 찾기
+      if (!resume) throw new Error("not found resume");
+
+      // 2. resume에서 유저데이터 가져오기
+      const user = resume.userId;
+      const { major, career, skill, region, interestJob, level } = user;
+
+      // 3. 유저데이터와 이력서 내용(sessions) gemini controller로 보내기
+      const roadmapData = await geminiController.generateRoadmapContent({
+        location: region,
+        interestJob: interestJob,
+      });
+      //   4. gemini res로 로드맵 생성
+      const newRoadmap = new Roadmap({
+        resumeId,
+        plans: roadmapData,
+      });
+
+      await newRoadmap.save();
+      res.status(200).json({ status: "success", data: newRoadmap });
+    } catch (error: any) {
+      res.status(400).json({ status: "fail", error: error.message });
+    }
+  },
+};
+
+export default roadmapController;
