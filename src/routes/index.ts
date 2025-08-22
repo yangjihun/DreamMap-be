1a243afc98c57022265ae3c28df10ce67cd3eeee
eunbin55 <eunbin620@naver.com>
2025-08-22 17:23:47 +0900

    feat: roadmap api router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,13 @@ import geminiApi from "@routes/gemini.api";
 import userApi from "./user.api";
 import authApi from "./auth.api";
 import resumeApi from "@routes/resume.api";
+import roadmapApi from "@routes/roadmap.api";
+
 const router = Router();
 
 router.use("/gemini", geminiApi);
 router.use("/user", userApi);
 router.use("/auth", authApi);
-router.use("/resume",resumeApi);
-
+router.use("/resume", resumeApi);
+router.use("/roadmap", roadmapApi);
 export default router;
-
